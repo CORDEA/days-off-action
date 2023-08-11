ec5c5ec3ae531dc9fcf78930a29122381c6b8053
Yoshihiro Tanaka <contact@cordea.jp>
2023-08-11 14:31:35 +0900

    check if the date is a holiday

--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 import * as core from "@actions/core";
+import Holidays from "date-holidays";
 
 async function main() {
-    const rawDate = core.getInput("date", {required: true});
-    const date = new Date(Date.parse(rawDate));
-    const country = core.getInput("country", {required: true});
-    const state = core.getInput("state");
-    const region = core.getInput("region");
-    const includeTypes = core.getInput("include-types").split(",");
-    const encoding = core.getInput("result-encoding");
+  const rawDate = core.getInput("date", { required: true });
+  const date = new Date(Date.parse(rawDate));
+  const country = core.getInput("country", { required: true });
+  const state = core.getInput("state");
+  const region = core.getInput("region");
+  const includeTypes = core.getInput("include-types").split(",");
+  const encoding = core.getInput("result-encoding");
+
+  const holidays = new Holidays(country, state, region);
+  const holiday = holidays.isHoliday(date);
+  switch (encoding ? encoding : "json") {
+    case "json":
+      core.setOutput(
+        "result",
+        JSON.stringify(holiday === false ? {} : holiday),
+      );
+      break;
+    case "bool":
+      core.setOutput("result", holiday !== false);
+      break;
+    default:
+      throw new Error("Unsupported encoding type.");
+  }
 }
 
 main().catch((e) => {
-    core.setFailed(`Got an unhandled error. ${e}`);
+  core.setFailed(`Got an unhandled error. ${e}`);
 });
